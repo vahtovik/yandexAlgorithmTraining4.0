c224875f1069dd9b2b83719c7e74e003039705c9
Artem Davtyan <artem_davtyan7@mail.ru>
2023-11-24 17:22:24 +0300

    task 4 2 B solution

---
+++ Lesson 2/task_B.py
@@ -0,0 +1,14 @@
+def prefix_function(s):
+    p = [0] * len(s)
+    for i in range(1, len(s)):
+        k = p[i - 1]
+        while k > 0 and s[k] != s[i]:
+            k = p[k - 1]
+        if s[k] == s[i]:
+            k += 1
+        p[i] = k
+    return p
+
+
+s = input()
+print(len(s) - prefix_function(s)[-1])
